8d665d2a3d00007a18b7389d4da56919f6375d04
Manpreet Singh <singh-manpreet@outlook.com>
2021-02-17 16:56:50 -0800

    Integrated MapParser into brain

--- src/brian/src/brian.cpp
+++ src/brian/src/brian.cpp
@@ -12,12 +12,14 @@
 #include <vector>
 #include <map>
 
-#include "brian/MapParser.h"
+#include "MapParser.h"
 
 using namespace std;
 
 float angle_min, angle_max, angle_increment, scan_time, range_min, range_max;
 vector<float> ranges, intensities;
+
+MapParser mapParser;
 
 void msgCallback(const sensor_msgs::LaserScan::ConstPtr & msg)
 {
@@ -30,11 +32,28 @@ void msgCallback(const sensor_msgs::LaserScan::ConstPtr & msg)
     ranges = msg->ranges;
     intensities = msg->intensities;
 
-    for (int i = 0; i < (int)(angle_max/angle_increment); i++)
-    {
-        printf("%f", ranges.at(i));
-    }
-    printf("\n");
+    // for (int i = 0; i < (int)(angle_max/angle_increment); i++)
+    // {
+    //     printf("%f", ranges.at(i));
+    // }
+    // printf("\n");
+}
+
+void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr & msg)
+{
+        mapParser.height = msg->info.height;
+        mapParser.width = msg->info.width;
+
+        mapParser.map_resolution = msg->info.resolution;
+
+        mapParser.map_data = msg->data;
+
+        mapParser.map_origin = msg->info.origin;
+
+        bool suc = mapParser.dump();
+
+        printf("Height: %d \tWidth: %d\n", mapParser.height, mapParser.width);
+        printf("%d\n", suc);
 }
 
 int main(int argc, char **argv)
@@ -42,12 +61,21 @@ int main(int argc, char **argv)
 
     ros::init(argc, argv, "brian");
 
+    
+
     geometry_msgs::Twist vel_msg;
     
     ros::NodeHandle n;
     ros::Subscriber lidar = n.subscribe("scan", 100, msgCallback);
 
+    ros::Subscriber map_sub = n.subscribe("map", 100, mapCallback);
+
+    MapParser mapParser;
+
     ros::spin();
+
+    cout << "Height: " << mapParser.height << "\t Width: " << mapParser.width << endl;
     // cout << "Hello, I am Brian" << endl;
+
     return 0;
 }
