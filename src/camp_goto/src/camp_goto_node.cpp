431bcb815618f280e0142b7ad90dde89d7a86aad
tjpigott <tjpigott@uw.edu>
2021-02-20 12:30:23 -0800

    Odometry-based goto position with basic obs. avoid

--- src/camp_goto/src/camp_goto_node.cpp
+++ src/camp_goto/src/camp_goto_node.cpp
@@ -9,10 +9,11 @@
 // ROS Default Header File
 #include <ros/ros.h>
 #include <sensor_msgs/LaserScan.h>
-#include <sensor_msgs/Imu.h>
+// #include <sensor_msgs/Imu.h>
 #include <geometry_msgs/Twist.h>
+#include <nav_msgs/Odometry.h>
 
-#include "localizer_dwm1001/Tag.h"
+// #include "localizer_dwm1001/Tag.h"
 
 #include <vector>
 #include <math.h>
@@ -24,14 +25,21 @@ using namespace std;
 //const int BURGER_MAX_ANG_VEL = 2.84;
 
 // somewhat safer constants because i am scared
-const int BURGER_MAX_LIN_VEL = 0.2; // 2 m/s
-const int BURGER_MAX_ANG_VEL = 1.5708; // pi/2 rad/s or 90 deg/s
+const float SCALE = 5;
+const float BURGER_MAX_LIN_VEL = 0.2/SCALE; // 0.2 m/s
+const float BURGER_MAX_ANG_VEL = 1.5708/SCALE; // pi/2 rad/s or 90 deg/s
 
-// Declare Scan variables globaly
-float angle_min, angle_max, angle_increment, scan_time, range_min, range_max;
-float tag_x=0, tag_y=0, heading;
+//Movement Vars
+float x_vel, z_ang_vel;
 float last_x=0, last_y=0;
+float long_x, long_y;
+float go_x, go_y, go_distance, heading;
+
+//Scan
+float angle_min, angle_max, angle_increment, scan_time, range_min, range_max;
 vector<float> ranges, intensities;
+// float tag_x=0, tag_y=0, w, x,y,z;
+float odom_x, odom_y;
 
 // These functions run if new info was published during a spin command.
 // They collects any data from the Scan, Imu, or Tag callbacks
@@ -46,19 +54,24 @@ void scanCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
     ranges = msg->ranges;
     intensities = msg->intensities;
 }
-void imuCallback(const sensor_msgs::Imu::ConstPtr& msg) {
-    //ROS_INFO("grabbing imu");
-    heading = msg->orientation.z;
+// void imuCallback(const sensor_msgs::Imu::ConstPtr& msg) {
+//     //ROS_INFO("grabbing imu");
+//     heading = msg->orientation.z;
+// }
+void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+    //ROS_INFO("grabbing odom");
+    odom_x = msg->pose.pose.position.x;
+    odom_y = msg->pose.pose.position.y;
 }
-void tagCallback(const localizer_dwm1001::Tag::ConstPtr& msg) {
-    //ROS_INFO("grabbing tag");
-    tag_x = msg->x;
-    tag_y = msg->y;
-}
+// void tagCallback(const localizer_dwm1001::Tag::ConstPtr& msg) {
+//     //ROS_INFO("grabbing tag");
+//     tag_x = msg->x;
+//     tag_y = msg->y;
+// }
 
 // This is the obstacle avoidance function. It is called while the robot is moving to a point to
 // check for things in the way and to avoid them. 
-// void obstacleAvoid() { // Change to return a 2d point (vector?)?
+void obstacleAvoid() {
     // ROS topic variables are already global, should be available for this function
     // I am going to write some pseudocode to help plan this function
     //--PSEUDOCODE--
@@ -69,11 +82,46 @@ void tagCallback(const localizer_dwm1001::Tag::ConstPtr& msg) {
         // return path around object
     // }
     // else return original path
-// }
+    int left = 0;
+    int right = 0;
+    float closest_front_object;
+    if(ranges.size() == 360){
+        // Find how close the closest object is (55 front scans)
+        if (ranges.at(0) != 0) closest_front_object = ranges.at(0);
+        else closest_front_object = 500;
+        for(int i = 1; i < 27; i++) {
+            if(ranges.at(i) < closest_front_object && ranges.at(i) != 0) closest_front_object = ranges.at(i);
+            if(ranges.at(360-i) < closest_front_object && ranges.at(360-i) != 0) closest_front_object = ranges.at(360-i);
+        }
+        if(closest_front_object<0.5) {
+            ROS_INFO("---FOUND---");
+            // Find most desirable direction, more points is better
+            for(int i = 1; i < 180; i++) {
+                if(ranges.at(i) < 1 && ranges.at(i) != 0) right++;
+                if(ranges.at(360-i) < 1 && ranges.at(360-i) != 0) left++;
+            }
+            if (right<left){
+                go_x = odom_x + cos(heading+PI/2);
+                go_y = odom_y + sin(heading+PI/2);
+            } else {
+                go_x = odom_x + cos(heading-PI/2);
+                go_y = odom_y + sin(heading-PI/2);
+            }
+        } else {
+            //Don't adjust course if no object in the way
+            go_x = long_x;
+            go_y = long_y;
+        }
+    } else {
+        //without LiDAR data don't move
+        go_x = odom_x;
+        go_y = odom_y;
+    }
+}
 
 // This function is an idea I have to determine movement between two 2D points, possibly like a
 // current position as determined from the dwm1001 or the kalman filter. 
-void pointToPoint(ros::Publisher cmd_publisher, float go_x, float go_y) {//accept a 2d point goal as a parameter and output a Twist message?
+void pointToPoint() {//accept a 2d point goal as a parameter and output a Twist message?
     //ROS_INFO("2p2 function");
     // Taking two points, both it's current position and either the long-term goal or
     // an intermediate step or obstacle correction point, this function determines
@@ -81,40 +129,38 @@ void pointToPoint(ros::Publisher cmd_publisher, float go_x, float go_y) {//accep
     geometry_msgs::Twist vel_msg;
 
     // calculate difference between current and previous positions (dx, dy)
-    float dx = tag_x - last_x;
-    float dy = tag_y - last_y;
+    float dx = odom_x - last_x;
+    float dy = odom_y - last_y;
     // calculate current heading using atan2 of  dx and dy
-    float current_heading = atan2(dy,dx);
+    ROS_INFO("dy/dx: (%f/%f)",dy,dx);
+    heading = atan2(dy,dx);
     // calculate the difference between the goal and the current position (gx, gy?)
-    float gx = go_x - tag_x;
-    float gy = go_x - tag_y;
+    float gx = go_x - odom_x;
+    float gy = go_y - odom_y;
     // find the deired heading by with atan2 of gx, gy
     float desired_heading = atan2(gy,gx);
     // find the distance remaining to travel
-    float go_distance = sqrt(gx*gx+gy*gy);
+    go_distance = sqrt(gx*gx+gy*gy);
     // find the error in the heading
-    float head_error = current_heading - desired_heading;
+    float head_error = desired_heading - heading;
     // correct the heading error so it is between -pi and pi
     if(head_error > PI) head_error = head_error - 2*PI;
     if(head_error < 0-PI) head_error = head_error + 2*PI;
+    ROS_INFO("\ndes: %f\nhed: %f\nerr: %f",desired_heading,heading,head_error);
     // control turn from heading error
-    float z_ang_vel = 2 * head_error; // 2 seems to be a good Kp in matlab sims
+    z_ang_vel = 2.0/SCALE * head_error; // 2 seems to be a good Kp in matlab sims
     // correct ang_vel so it is within acceptable margins    
     if(z_ang_vel > BURGER_MAX_ANG_VEL) z_ang_vel = BURGER_MAX_ANG_VEL;
-    float x_vel = BURGER_MAX_LIN_VEL; //goes fast! might be good to slow down near detected objects, but i didn't make that yet.
-    if(go_distance < 0.1) x_vel = 0; //stop if at a final point
-    // Write to the vel_msg we plan to publish
-	vel_msg.linear.x = x_vel;
-	vel_msg.linear.y = 0;
-	vel_msg.linear.z = 0;
-	vel_msg.angular.x = 0;
-	vel_msg.angular.y = 0;
-	vel_msg.angular.z = z_ang_vel;
-    //publish vel_msg (move robot)
-	cmd_publisher.publish(vel_msg);
+    if(z_ang_vel < -BURGER_MAX_ANG_VEL) z_ang_vel = -BURGER_MAX_ANG_VEL;
+    x_vel = BURGER_MAX_LIN_VEL; //goes fast! might be good to slow down near detected objects, but i didn't make that yet.
+    if(go_distance < 0.03) {
+        //stop if at a final point
+        x_vel = 0; 
+        z_ang_vel = 0;
+    }
     //save this for future calculations: (needs fenceposting?)
-    last_x = tag_x;
-    last_y = tag_y;
+    last_x = odom_x;
+    last_y = odom_y;
 }
 
 // This is the main function.
@@ -134,19 +180,35 @@ int main(int argc, char **argv){
 
 	// scan_subscriber subscribes to the lidar's scan, cmd_publisher publishes a twist, and the size of the publisher queue is set to 10.
 	ros::Subscriber scan_subscriber = nh.subscribe("scan", 10, scanCallback);
-    ros::Subscriber imu_subscriber = nh.subscribe("imu", 10, imuCallback);
-    ros::Subscriber tag_subscriber = nh.subscribe("dwm1001/tag1", 10, tagCallback);
+    // ros::Subscriber imu_subscriber = nh.subscribe("imu", 10, imuCallback);
+    ros::Subscriber odom_subscriber = nh.subscribe("odom", 10, odomCallback);
+    // ros::Subscriber tag_subscriber = nh.subscribe("dwm1001/tag1", 10, tagCallback);
 	ros::Publisher cmd_publisher = nh.advertise<geometry_msgs::Twist>("cmd_vel", 10);
     	
 	while(ros::ok()) {
         //Check inputs:
-        ROS_INFO("heading: %f", heading);
+        //ROS_INFO("orientation:\nw:%f\nx:%f\ny:%f\nz:%f", w,x,y,z);
         //ROS_INFO("x: %f Y:%f", tag_x, tag_y);
 
-
-
-       
-        //pointToPoint(cmd_publisher,1,1);
+        long_x = 2;
+        long_y = -0.1;
+        //check for obstruction and set go vars
+        obstacleAvoid();
+        ROS_INFO("\nAt: %f, %f\nGo: %f, %f\nDistance: %f",odom_x,odom_y,go_x,go_y,go_distance);
+        //Determine motor instructions from current point and the go vars
+        pointToPoint();
+        ROS_INFO("X: %f  Z: %f",x_vel, z_ang_vel);
+        
+        // Write to the vel_msg we plan to publish
+	    vel_msg.linear.x = x_vel;
+	    vel_msg.linear.y = 0;
+	    vel_msg.linear.z = 0;
+	    vel_msg.angular.x = 0;
+	    vel_msg.angular.y = 0;
+	    vel_msg.angular.z = z_ang_vel;
+	    //vel_msg.angular.z = 0;
+        //publish vel_msg (move robot)
+	    cmd_publisher.publish(vel_msg);
         // Sleep according to the loop rate above
 		loop_rate.sleep();
         // Check for new messages from subscribed nodes
