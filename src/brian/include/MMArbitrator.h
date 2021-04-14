1b2dd2d0dbf4854198b7693bff30656d4782d251
manpreet-singh <manpreetbassi96@gmail.com>
2021-04-14 00:23:14 -0700

    Fixed additional issues with MMA

--- src/brian/include/MMArbitrator.h
+++ src/brian/include/MMArbitrator.h
@@ -8,14 +8,30 @@
  * robot.
  */ 
 
+// ROS Objects
 #include <ros/node_handle.h>
 #include <ros/subscriber.h>
 #include <ros/publisher.h>
 #include <ros/service_client.h>
+
+// ROS Messages
+#include <geometry_msgs/Twist.h>
+#include <nav_msgs/OccupancyGrid.h>
+
+// Standard C/C++ Libraries
 #include <vector>
+#include <string>
+#include <boost/bind.hpp>
 
+// Development Libraries from within this project
 #include "fkie_multimaster_msgs/DiscoverMasters.h"
+#include "fkie_multimaster_msgs/MasterState.h"
 
+/**
+ * @file MMArbitator.h
+ * @brief Arbitrator object to publish and subsribe to new data available on the network
+ * @author Manpreet Singh (manpreet-singh)
+ */
 class MMArbitrator
 {
 public:
@@ -35,7 +51,12 @@ private:
 
     // Multimaster entry point, this is how this system
     // finds other robots on the network
-    ros::Subscriber _mm_entry_point;
+    ros::ServiceClient _mm_entry_point;
+    // DiscoverMasters service data structure
+    fkie_multimaster_msgs::DiscoverMasters _fkie_service;
+
+    // Information about the system running this node
+    ros::Subscriber _master_state;
 
     // Subscriber to the current robot odometry to later publish
     ros::Subscriber _odom;
@@ -43,15 +64,28 @@ private:
     // Subscribe to the current robot map to later publish
     ros::Subscriber _map_sub;
 
+    // List of other hosts on the network
+    std::vector<std::string> _available;
+
+    // List of positions of all hosts on the network
+    std::vector<geometry_msgs::Twist> _positions;
+
+    // List of maps of all hosts on the network 
+    std::vector<nav_msgs::OccupancyGrid> _maps;
+
+    std::string _current_name = "";
+
     // Odometry sub callback 
-    void position_callback();
+
+    void position_callback(int foo, const geometry_msgs::Twist::ConstPtr& pos);
 
     // Map sub callback
-    void map_callback();
 
-    // Multimaster callback
-    void mm_callback();
-    
+    void map_callback(int foo, const nav_msgs::OccupancyGrid::ConstPtr& map);
+
+    // MasterState callback
+
+    void master_state_callback(const fkie_multimaster_msgs::MasterState::ConstPtr& ms);
 };
 
 #endif // MM_ARBITRATOR_H
--- src/brian/src/MMArbitrator.cpp
+++ src/brian/src/MMArbitrator.cpp
@@ -7,19 +7,27 @@
  */
 MMArbitrator::MMArbitrator(ros::NodeHandle n) 
 {
-    // Subscribe to the multimaster entry point
-    // this->_mm_entry_point = n.subscribe();
+    // Setup client for the multimaster entry point
+    this->_mm_entry_point = n.serviceClient<fkie_multimaster_msgs::DiscoverMasters>("master_discovery/list_masters");
+    this->_master_state = n.subscribe("master_discovery/changes", 10, &MMArbitrator::master_state_callback, this);
+    ros::spinOnce();
 
+    
     // Subscribe to the current robot's map and odometry
     // this->_odom = n.subscribe();
     // this->_map_sub = n.subscribe();
 
-    ros::ServiceClient client = n.serviceClient<fkie_multimaster_msgs::DiscoverMasters>("master_discovery/list_masters");
-
-    fkie_multimaster_msgs::DiscoverMasters srv;
-    if (client.call(srv))
-        ROS_INFO("ROS Master 0: %s", srv.response.masters.at(0).name.c_str());
-    
+    if (this->_mm_entry_point.call(this->_fkie_service))
+    {
+        // ROS_INFO("ROS Master 0: %s", srv.response.masters.at(0).name.c_str());
+        int s = this->_fkie_service.response.masters.size();
+        for (int i = 0; i < s; i++)
+        {
+            // Print name and push to available hosts vector
+            ROS_INFO("ROS Master %d : %s", i, this->_fkie_service.response.masters.at(i).name.c_str());
+            this->_available.push_back(this->_fkie_service.response.masters.at(i).name.c_str());
+        }
+    }
 
     // ROS_INFO("Masters: %s", response.masters.at(0).name.c_str());
 
@@ -28,26 +36,46 @@ MMArbitrator::MMArbitrator(ros::NodeHandle n)
 /**
  * @brief Sync the current nodes with what is available on the network
  * 
- * @param n 
+ * @param n NodeHandle object to use to update the parameters of this class
  */
 void MMArbitrator::sync(ros::NodeHandle n)
 {
     
 }
 
-void MMArbitrator::position_callback()
+/**
+ * @brief Callback function for the positions subscribers
+ * 
+ * @param foo Index of the object to update
+ * @param pos Twist object
+ */
+void MMArbitrator::position_callback(int foo, const geometry_msgs::Twist::ConstPtr& pos)
 {}
 
-void MMArbitrator::map_callback()
+/**
+ * @brief Callback function for the maps subscribers
+ * 
+ * @param foo Index of the object to update
+ * @param map OccupancyGrid object
+ */
+void MMArbitrator::map_callback(int foo, const nav_msgs::OccupancyGrid::ConstPtr& map)
 {}
 
-void MMArbitrator::mm_callback()
-{}
+/**
+ * @brief Collect the name of the current master from the system
+ * 
+ * @param ms MasterState object
+ */
+void MMArbitrator::master_state_callback(const fkie_multimaster_msgs::MasterState::ConstPtr& ms)
+{ this->_current_name = ms->master.name.c_str(); }
 
 /**
  * @brief Class Destructor
  */
 MMArbitrator::~MMArbitrator()
 {
+    this->_available.clear();
+    this->_positions.clear();
+    this->_maps.clear();
     
 }
