68d3a3bea322388cde79a86f2581d3b335d6a6c8
manpreet-singh <manpreetbassi96@gmail.com>
2021-04-15 17:26:02 -0700

    Untested calibration code translated from MATLAB

--- src/camp_goto/src/camp_goto_calibration.cpp
+++ src/camp_goto/src/camp_goto_calibration.cpp
@@ -5,41 +5,141 @@ Started 4/15/2021 by Tyler Pigott
 
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Vector3.h>
 #include <nav_msgs/Odometry.h>
 #include <vector>
 #include <math.h>
+#define PI 3.14159265
 
 using namespace std;
 
+const int min_point_count = 10;
+const float bad_theta_threshold = 100;
+const float bad_data_threshold = 5;
+const float theta_found_threshold = 0.001;
+const float d_theta = 0.001;
+float theta_0 = 0;
+
 vector<float> odom_x, odom_y, deca_x, deca_y;
+float odom_in_x,odom_in_y,deca_in_x,deca_in_y;
 
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
     //ROS_INFO("grabbing odom");
-    odom_x.push_back(msg->pose.pose.position.x);
-    odom_y.push_back(msg->pose.pose.position.y);
+    odom_in_x = msg->pose.pose.position.x;
+    odom_in_y = msg->pose.pose.position.y;
 }
 void tagCallback(const geometry_msgs::Twist::ConstPtr& msg) {
     //ROS_INFO("grabbing tag");
-    deca_x.push_back(msg->linear.x);
-    deca_y.push_back(msg->linear.y);
+    deca_in_x = msg->linear.x;
+    deca_in_y = msg->linear.y;
+}
+
+void pushCoords(){
+    odom_x.push_back(odom_in_x);
+    odom_y.push_back(odom_in_y);
+    deca_x.push_back(deca_in_x);
+    deca_y.push_back(deca_in_y);
+}
+void clearCoords(){
+    odom_x.clear();
+    odom_y.clear();
+    deca_x.clear();
+    deca_y.clear();
+}
+
+void publishOffsets(ros::Publisher transform_publisher){
+    geometry_msgs::Vector3 tr;
+    float tx,ty,theta,last_theta,run;
+    float a,b,a2,b2,tx2,ty2,n,f,f2,f_prime,e;
+
+    theta = theta_0;
+    last_theta = theta_0;
+    run = 1;
+
+    while(run == 1){
+        run = 0;
+        a = cos(theta);
+        b = sin(theta);
+        a2 = cos(theta + d_theta);
+        b2 = sin(theta + d_theta);
+        tx = 0;
+        ty = 0;
+        tx2 = 0;
+        ty2 = 0;
+        n = odom_x.size();
+        for(int i = 0; i < n; i++){
+            tx = tx + (1/n)*(deca_x[i]-a*odom_x[i]+b*odom_y[i]);
+            ty = ty + (1/n)*(deca_y[i]-a*odom_y[i]-b*odom_x[i]);
+            tx2 = tx2 + (1/n)*(deca_x[i]-a2*odom_x[i]+b2*odom_y[i]);
+            ty2 = ty2 + (1/n)*(deca_y[i]-a2*odom_y[i]-b2*odom_x[i]);
+        }
+        f = 0;
+        f2 = 0;
+        for(int i = 0; i < n; i++){
+            f = f + (ty-deca_y[i])*(a*odom_x[i]-b*odom_y[i]) - (tx-deca_x[i])*(b*odom_x[i]+a*odom_y[i]);
+            f2 = f2 + (ty2-deca_y[i])*(a2*odom_x[i]-b2*odom_y[i]) - (tx2-deca_x[i])*(b2*odom_x[i]+a2*odom_y[i]);
+        }
+        f_prime = (f2 - f)/d_theta;
+        last_theta = theta;
+        theta = theta - f/f_prime;
+        if (theta > PI || theta < -PI) theta = theta - floor(theta/(PI))*PI;
+        if (abs((theta - last_theta)/theta) > theta_found_threshold) 
+            run = 1; 
+        else {
+            e = 0;
+            for(int i = 0; i < n; i++){
+                e = e + + pow((a*odom_x[i]-b*odom_y[i]+tx-deca_x[i]),2)+pow((b*odom_x[i]+a*odom_y[i]+ty-deca_y[i]),2);
+            }
+            if(e > bad_theta_threshold) run = 1;
+            //else if(e > bad_data_threshold) do something that fixes it i guess? throw everything out?
+        }
+
+    }
+
+    theta_0 = theta;
+
+    tr.x = tx;
+    tr.y = ty;
+    tr.z = theta;
+    ROS_INFO("pos offsets: (%f/%f)",tx,ty);
+    ROS_INFO("Theta: %f",theta);
+	transform_publisher.publish(tr);
 }
 
 int main(int argc, char **argv){
 	ros::init(argc, argv, "calibrator");
 	ros::NodeHandle nh;	
-    //camp_goto::transform tr;
-	ros::Rate loop_rate(1);
+	ros::Rate loop_rate(10);
     
     ros::Subscriber odom_subscriber = nh.subscribe("odom", 10, odomCallback);
     ros::Subscriber tag_subscriber = nh.subscribe("filtered", 10, tagCallback);
-	//ros::Publisher transform_publisher = nh.advertise<camp_goto::transform>("transform", 10);
+	ros::Publisher transform_publisher = nh.advertise<geometry_msgs::Vector3>("transform", 10);
     
     while(ros::ok()) {
         
         // I bet something real fancy goes here
+        // Collect data when it comes in
+        // Save some data a distance apart from each other
+        // with a certian threshhold crossed, calculate the offsets
+        // publish them
 
-	    //transform_publisher.publish(tr);
+        // Do this if there are no point syet
+        if(odom_x.size() == 0) {
+            pushCoords();
+        }
+
+        // Do this if its time for another point
+        if(sqrt(pow((odom_x.back() - odom_in_x),2) + pow((odom_y.back() - odom_in_y),2))>0.1) {
+            pushCoords();
+        }
+
+        // Do this if there are enough points to do a conversion
+        if(odom_x.size() >= min_point_count){
+            publishOffsets(transform_publisher);
+            clearCoords();
+        }
+
 		loop_rate.sleep();
 		ros::spinOnce();
 	}
