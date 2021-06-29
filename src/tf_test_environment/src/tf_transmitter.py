e876ac7594710c4ea126a84274a3fe7ca6a2d901
stcarda <seancarda@msn.com>
2021-06-29 16:50:18 -0700

    TF Changes.

---
+++ src/tf_test_environment/src/tf_transmitter.py
@@ -0,0 +1,44 @@
+#---------------------------------------
+# Sean Carda
+# 6/29/21
+# Project: CAMP
+# TF2 Transmitter Script
+#---------------------------------------
+
+# STEPS:
+# 1) Verify that transforms can be performed without errors.
+
+
+import math
+import roslib
+import rospy
+import sys
+roslib.load_manifest('rospy')
+
+import tf
+import tf2_geometry_msgs
+import tf2_msgs
+import tf2_py
+import tf2_ros
+
+class TF_Transmitter:
+
+    def __init__(self):
+        print("This is temporary")
+
+
+    def main(self):
+        print("This is temprary")
+
+        def publish_points(self):
+            print("This is temperary")
+
+        publish_points()
+
+
+if __name__ == '__main__':
+    code = TF_Transmitter()
+    while not rospy.is_shutdown:
+        code.main()
+        rospy.sleep(0.1)
+
