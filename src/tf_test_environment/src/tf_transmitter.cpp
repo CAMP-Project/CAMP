6ac99d9841508bb0a08e6e984d032fa97e5e285e
stcarda <seancarda@msn.com>
2021-07-05 19:06:06 -0700

    More work on tf. Working on verifying the results of a tf transform. Running into "lookup transform" bugs.

--- src/tf_test_environment/src/tf_transmitter.cpp
+++ src/tf_test_environment/src/tf_transmitter.cpp
@@ -8,8 +8,12 @@
 // TF2 inclusions.
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_ros/transform_broadcaster.h>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 #include <geometry_msgs/TransformStamped.h>
 #include <geometry_msgs/PointStamped.h>
+#include <geometry_msgs/PoseStamped.h>
 #include <turtlesim/Pose.h>
 //--------------------------------------------
 // Normal C++ Inclusions.
@@ -39,12 +43,19 @@ float point_origin_y = 2.0;
 
 // Initialize a transformStamped object.
 geometry_msgs::TransformStamped transformStamped;
+geometry_msgs::TransformStamped odom2PointTransform;
 
 // Publisher declaration.
 ros::Publisher point_publisher;
 
 // Info for displaying the point in RVIZ.
 geometry_msgs::PointStamped test_point;
+geometry_msgs::PoseStamped roboPosition;
+geometry_msgs::PoseStamped roboInPointPose;
+
+// TF2 Buffer.
+tf2_ros::Buffer tfb;
+
 
 // Callback for robot odometry.
 void odomCallback(const nav_msgs::Odometry::ConstPtr& msg) {
@@ -57,7 +68,7 @@ void establishTransformInfo() {
     static tf2_ros::TransformBroadcaster br;
 
     transformStamped.header.stamp = ros::Time::now();
-    transformStamped.header.frame_id = "test_point";
+    transformStamped.header.frame_id = "map";
     transformStamped.child_frame_id = "point";
     transformStamped.transform.translation.x = point_origin_x;
     transformStamped.transform.translation.y = point_origin_y;
@@ -85,6 +96,9 @@ int main(int argc, char** argv) {
     // Point publisher.
     point_publisher = nh.advertise<geometry_msgs::PointStamped>("Test_Point", 10);
 
+    tf2_ros::Buffer tfBuffer;
+    tf2_ros::TransformListener tfListener(tfBuffer);
+
     // Functionality.
     while(ros::ok()) {
 
@@ -94,20 +108,46 @@ int main(int argc, char** argv) {
         test_point.point.y = 2.0;
         test_point.point.z = 2.0;
         test_point.header.stamp = ros::Time::now();
-        test_point.header.frame_id = "test_point";
+        test_point.header.frame_id = "map";
 
+        // Establishes robot positional information for transform.
+        geometry_msgs::PoseStamped roboPosition;
+        roboPosition.header.stamp = ros::Time::now();
+        roboPosition.header.frame_id = "odom";
+        roboPosition.pose.position.x = odom_x;
+        roboPosition.pose.position.y = odom_y;
+        roboPosition.pose.position.z = 0.0;
+        roboPosition.pose.orientation.x = 0.0;
+        roboPosition.pose.orientation.y = 0.0;
+        roboPosition.pose.orientation.z = 0.0;
+        roboPosition.pose.orientation.w = 0.0;
+
+        // Establish transform info from map to point. Also, publish the position of the point to RVIZ.
         establishTransformInfo();
         point_publisher.publish(test_point);
 
 
-        // NEED A BLOCK FOR TRANSFORM CALCULATION WITH TF***
+        // Instantiation of a transformStamped to get the transform between odometry and the test point.
+        // Also, instantiated a new poseStamped for storing the result of the doTransform method.
+        geometry_msgs::TransformStamped odom2PointTransform;
+        geometry_msgs::PoseStamped roboInPointPose;
+        try {
+            odom2PointTransform = tfb.lookupTransform("odom", "point", ros::Time(0));
+            tf2::doTransform(roboPosition, roboInPointPose, odom2PointTransform);
+        } catch (tf2::TransformException &ex) {
+            ROS_WARN("%s", ex.what());
+            ros::Duration(1.0).sleep();
+            continue;
+        }
 
+        // Calculate the true distance between the robot and the test point. Compare to the result from tf2.
+        float true_x = odom_x - point_origin_x;
+        float true_y = odom_y - point_origin_y;
+        float tf_x = roboInPointPose.pose.position.x;
+        float tf_y = roboInPointPose.pose.position.y;
 
-
-        // NEED TO CALCULATE TRUE POSITION WITH MATH***
-
+        ROS_INFO("-------True Results-------\nTrue x:   %2.3f   True y:   %2.3f\n-------TF_2 Results-------\nTF2 x:   %2.3f   TF2 y:   %2.3f", true_x, true_y, tf_x, tf_y);
 
     }
-
     return 0;
 }
