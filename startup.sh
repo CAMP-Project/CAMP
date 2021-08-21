c88a932c52ccce5399dfc90e276c2b75c625f1ca
manpreet-singh <manpreetbassi96@gmail.com>
2021-08-20 18:39:39 -0700

    Trying Fix

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 source devel/setup.bash 
-# TODO: Add additinal laaunch commands to this startup file
+# TODO: Add additinal launch commands to this startup file
 
 export TURTLEBOT3_MODEL='burger'
 sudo chmod 777 /dev/ttyACM0
 sudo chmod 777 /dev/ttyACM1
-#roslaunch brian brian.launch
+roslaunch brian brian.launch
